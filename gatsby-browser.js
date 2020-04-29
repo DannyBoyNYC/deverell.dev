ceabe7d1bef96504ea8a83ba7145a15ef1f4513a
DanielDeverell <daniel@getperksy.com>
2020-04-29 08:17:51 -0400

    removed old posts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,4 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 import "prismjs/themes/prism.css"
+// "Open Sans", sans-serif;
--- gatsby-config.js
+++ gatsby-config.js
@@ -51,8 +51,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Deverell dot dev`,
+        short_name: `deverell`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
